a83b93b7f11e51868091a16a3ad2702dfa63c510
yoeldovidcohen <46816177+yoeldovidcohen@users.noreply.github.com>
2023-12-08 13:45:37 +0000

    a

--- App.tsx
+++ App.tsx
@@ -1,13 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import * as Location from 'expo-location';
+
+
 
 export default function App() {
+
+  const [errorMsg, setErrorMsg] = useState<string | null>(null)
+  const [compassHeading, setComppassHeading] = useState<Location.LocationHeadingObject | null>(null)
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync()
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied")
+        return
+      }
+
+      // const location = await Location.getCurrentPositionAsync({})
+      // setLocation(location)
+
+      // const compl = await Location.getHeadingAsync()
+      Location.watchHeadingAsync((cc) => setComppassHeading(cc))
+      // setCompp(compl)
+    })()
+  }, [])
+
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>asdf</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
